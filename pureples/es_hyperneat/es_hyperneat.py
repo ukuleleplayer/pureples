b7597f081548078c331f660878b45c682f941790
yamatakeru <yamato_y@me.com>
2021-06-07 12:58:14 +0900

    Add function of comparison of type to Connection’s __eq__.

--- pureples/es_hyperneat/es_hyperneat.py
+++ pureples/es_hyperneat/es_hyperneat.py
@@ -241,7 +241,9 @@ class Connection:
         self.weight = weight
 
     # Below is needed for use in set.
-    def __eq__(self,other):
+    def __eq__(self, other):
+        if type(other) is not Connection:
+            return NotImplemented
         return (self.x1, self.y1, self.x2, self.y2) == (other.x1, other.y1, other.x2, other.y2)
 
     def __hash__(self):
